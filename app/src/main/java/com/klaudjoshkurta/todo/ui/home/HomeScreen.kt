def84c035b740b187b53a2b42b66e30df429c163
ShkurtaKlaudjo <shkurtaklaudjo@gmail.com>
2024-07-24 00:10:56 +0300

    Wip

---
+++ app/src/main/java/com/klaudjoshkurta/todo/ui/home/HomeScreen.kt
@@ -0,0 +1,83 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.klaudjoshkurta.todo.ui.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.klaudjoshkurta.todo.ui.theme.TodoTheme
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { /*TODO*/ },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Rounded.Menu, contentDescription = null)
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(200.dp),
+                containerColor = Color.Black,
+                contentColor = Color.White,
+                modifier = Modifier.padding(bottom = 60.dp)
+            ) {
+                Icon(imageVector = Icons.Rounded.Add, contentDescription = null)
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 60.dp),
+                text = "Good morning, Klaudjo",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    TodoTheme {
+        HomeScreen()
+    }
+}
---
+++ app/src/main/java/com/klaudjoshkurta/todo/ui/home/composables/TodoItem.kt
@@ -0,0 +1,18 @@
+package com.klaudjoshkurta.todo.ui.home.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.klaudjoshkurta.todo.ui.theme.TodoTheme
+
+@Composable
+fun TodoItem() {
+
+}
+
+@Preview(widthDp = 412, showBackground = true)
+@Composable
+fun TodoItemPreview() {
+    TodoTheme {
+        TodoItem()
+    }
+}
